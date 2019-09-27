8b04ab9ff50ea8a1912b63215543e0055415bfc4
sbuvaneish@gmail.com <ec2-user@ip-172-31-41-143.us-east-2.compute.internal>
2019-09-27 18:11:24 +0000

    db creds

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
-$server = 'localhost';
-$username = 'admin';
-$password = '';
-$database = 'marketplace';
+$server = 'us-cdbr-iron-east-02.cleardb.net;
+$username = 'b7e32c44881d1e';
+$password = '5550f55b';
+$database = 'heroku_2f6114e386eb6eb';
 
 try{
 	$conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
