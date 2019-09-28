b051aab1c1aa78901db913e54ea611aebcea387e
sbuvaneish@gmail.com <ec2-user@ip-172-31-41-143.us-east-2.compute.internal>
2019-09-28 16:39:12 +0000

    Modified column name according to users table in login.php

--- login.php
+++ login.php
@@ -10,7 +10,7 @@ require 'database.php';
 
 if(!empty($_POST['email']) && !empty($_POST['password'])):
 	
-	$records = $conn->prepare('SELECT id,email,password FROM users WHERE email = :email');
+	$records = $conn->prepare('SELECT id,username,password FROM users WHERE username = :email');
 	$records->bindParam(':email', $_POST['email']);
 	$records->execute();
 	$results = $records->fetch(PDO::FETCH_ASSOC);
