640d8e9ab898847253e84f7eaca26016d136b526
sbuvaneish@gmail.com <ec2-user@ip-172-31-41-143.us-east-2.compute.internal>
2019-10-05 20:16:04 +0000

    image

--- image.php
+++ image.php
@@ -1,8 +1,8 @@
 <?php
-  $host = "$servername = "us-cdbr-iron-east-02.cleardb.net";
+  $host = "us-cdbr-iron-east-02.cleardb.net";
   $username = "b7e32c44881d1e";
   $password = "5550f55b";
-  $dbname = "heroku_2f6114e386eb6eb";";
+  $dbname = "heroku_2f6114e386eb6eb";
   $dbh = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
   $dbh->exec("SET NAMES utf8");
   $q = $dbh->prepare("select image from images where id = 2");
